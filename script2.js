95bc20b2954307ef1bdf237cd27798d277fd7773
RdkSki <rotax@gmx.net>
2021-09-23 15:04:49 +0200

    advanced including arrow function

--- script2.js
+++ script2.js
@@ -1,3 +1,24 @@
-function timeConverter(){
+/*function timeConverter(min){
+let hh = Math.floor(min/60);
+let mm = min - (hh*60);
+return `${min} minutes = ${hh}  hour(s) and ${mm} minute(s).`;
+}
 
-};
+console.log(timeConverter(560)); 
+*/
+
+//var x = prompt("are you ready?");   pop up window for var x !
+// console.log(functionName(insert arguments for parameters here));
+
+
+/*  const x = () => {
+
+}; */
+
+const timeCon = min => {
+    let hh = Math.floor(min/60);
+    let mm = min - (hh*60);
+    return `${min} minutes = ${hh}  hour(s) and ${mm} minute(s).`
+    };
+console.log(timeCon(2890));
+   
