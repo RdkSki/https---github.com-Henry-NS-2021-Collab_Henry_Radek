f9b1c5905c2994044652afd15aac4dcc9cb7b498
RdkSki <rotax@gmx.net>
2021-09-23 10:35:41 +0200

    basic1

---
+++ script1.js
@@ -0,0 +1,19 @@
+/*function printAvgMult(a, b, c){
+    let avg = (a + b + c)/3;
+    let mult = (a * b * c);
+    return avg; //return [avg, mult];
+}
+let result = printAvg(2, 7, 5); //let [avg, mult] = printAvgMult(2, 7, 5)
+console.log = (result); */
+
+function CrystalGazer(a, b, c, d){
+    let text = `You will be a ${c} in ${d} and married to ${a} with ${b} children.`;;
+    return text;
+}
+
+let result = CrystalGazer("Mary", "three", "Preacher", "Mistelbach");
+console.log(result);
+
+/*function ageCalculator(e){
+    let screen = `your age is ${e} or ${e+1}`;
+}*/
