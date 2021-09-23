4a03d067d38a1f9951f8b83e42e2f9e5d7c7d647
RdkSki <rotax@gmx.net>
2021-09-23 12:07:51 +0200

    basics and inter finished

--- script1.js
+++ script1.js
@@ -1,20 +1,41 @@
-/*function printAvgMult(a, b, c){
-    let avg = (a + b + c)/3;
-    let mult = (a * b * c);
-    return avg; //return [avg, mult];
-}
-let result = printAvg(2, 7, 5); //let [avg, mult] = printAvgMult(2, 7, 5)
-console.log = (result); */
-
-function CrystalGazer(a, b, c, d){
+/*function CrystalGazer(a, b, c, d){
     let text = `You will be a ${c} in ${d} and married to ${a} with ${b} children.`;;
     return text;
 }
 
 let result = CrystalGazer("Mary", "three", "Preacher", "Mistelbach");
-console.log(result);
+console.log(result);*/
 
-/*function ageCalculator(e){
-    let screen = `your age is ${e} or ${e+1}`;
-}*/
+/*function ageCalculator(f){
+    let e = 2021 - f;
+    return `your age is ${e} or ${e-1}`;
+}
 
+let x = ageCalculator(2000); 
+
+console.log(x);*/
+
+/*function AreVol(a, b, c){
+    let area = (a * b);
+    let volume = (a * b * c);
+    return  `The area of the box is: ${area} The volume of the box is: ${volume}`;
+     //return [avg, mult];
+}
+let result = AreVol(2, 7, 5); //let [avg, mult] = printAvgMult(2, 7, 5)
+console.log(result); */
+
+/*const x = (function() {
+    let text1 = `i am a web developer`;
+    let text2 = text1.charAt(0).toUpperCase()+text1.substring(1,20);
+    return text2;
+}());
+
+console.log(x);*/
+
+/*function Average(Math, Physics, English){
+    let sum = (Math+Physics+English);
+    let avg = sum/3;
+    return  `Sum: ${sum} Average: ${avg}`;
+}
+let result = Average(3, 4, 5); 
+console.log(result);*/
